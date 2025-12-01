da30dcdb18a64fab5a1e1dfdf07db8e8b55fa69c
MIKObscura <contact.mikobscura@gmail.com>
2025-12-01 12:16:47 +0100

    added picker script

---
+++ picker.py
@@ -0,0 +1,34 @@
+import random
+
+langs = [
+    "python",
+    "ruby",
+    "C",
+    "C++",
+    "Objective-C",
+    "C#",
+    "squirrel",
+    "haskell",
+    "erlang",
+    "elixir",
+    "julia",
+    "r",
+    "fortran",
+    "java",
+    "kotlin",
+    "clojure",
+    "javascript",
+    "PHP",
+    "crystal",
+    "lobster",
+    "zig",
+    "lua",
+    "smalltalk",
+    "swift",
+    "go",
+    "rust",
+    "dart",
+    "D"
+]
+
+print(random.choice(langs))
