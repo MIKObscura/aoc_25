1a9b8def7eaa01bedb95cb72e6afcf81298b32a7
MIKObscura <contact.mikobscura@gmail.com>
2025-12-07 17:42:14 +0100

    day 4 part 1

---
+++ day4/day4.kt
@@ -0,0 +1,31 @@
+import java.io.File
+import java.util.Arrays
+
+fun is_forklift_certified(pos: Pair<Int, Int>, grid: List<CharArray>): Boolean{
+    var neighbouring_rolls = 0
+    for(i in -1..1){
+        for(ii in -1..1){
+            val neighbour: Pair<Int, Int> = Pair(pos.first + i, pos.second + ii)
+            if(i == 0 && ii == 0) continue
+            if(neighbour.first < 0 || neighbour.second < 0) continue;
+            if(neighbour.first >= grid.size || neighbour.second >= grid[0].size) continue;
+            if(grid[neighbour.first][neighbour.second] == '@') neighbouring_rolls += 1
+        }
+    }
+    return neighbouring_rolls < 4
+}
+
+fun main(args: Array<String>){
+    val input_file = File("input.txt").readText()
+    val lines = input_file.split("\n")
+    val input = lines.map { l: String -> l.toCharArray() }
+    var accessibles: Array<Pair<Int, Int>> = emptyArray()
+    for(x in 0..(input.size - 1)){
+        for(y in 0..(input[0].size - 1)){
+            val pos = Pair(x, y)
+            if(input[x][y] != '@') continue
+            if(is_forklift_certified(pos, input)) accessibles += pos
+        }
+    }
+    println(accessibles.size)
+}
