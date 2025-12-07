e30945d6bd5ba3e21c255d71bb49d97165ede24d
MIKObscura <contact.mikobscura@gmail.com>
2025-12-07 17:51:54 +0100

    day 4 part 2

--- day4/day4.kt
+++ day4/day4.kt
@@ -15,12 +15,18 @@ fun is_forklift_certified(pos: Pair<Int, Int>, grid: List<CharArray>): Boolean{
     return neighbouring_rolls < 4
 }
 
+fun remove_rolls(rolls: Array<Pair<Int, Int>>, grid: List<CharArray>){
+    for(r in rolls){
+        grid[r.first][r.second] = '.'
+    }
+}
+
 fun main(args: Array<String>){
     val input_file = File("input.txt").readText()
     val lines = input_file.split("\n")
     val input = lines.map { l: String -> l.toCharArray() }
     var accessibles: Array<Pair<Int, Int>> = emptyArray()
-    for(x in 0..(input.size - 1)){
+    for(x in 0..(input.size - 1)){ // part 1
         for(y in 0..(input[0].size - 1)){
             val pos = Pair(x, y)
             if(input[x][y] != '@') continue
@@ -28,4 +34,19 @@ fun main(args: Array<String>){
         }
     }
     println(accessibles.size)
+    var total_removed = 0
+    while(true){ // part 2
+        var accessibles: Array<Pair<Int, Int>> = emptyArray()
+        for(x in 0..(input.size - 1)){
+            for(y in 0..(input[0].size - 1)){
+                val pos = Pair(x, y)
+                if(input[x][y] != '@') continue
+                if(is_forklift_certified(pos, input)) accessibles += pos
+            }
+        }
+        if(accessibles.size == 0) break
+        total_removed += accessibles.size
+        remove_rolls(accessibles, input)
+    }
+    println(total_removed)
 }
