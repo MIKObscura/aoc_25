07ea6e9a302b6480faf64d1630568917339e76e0
MIKObscura <contact.mikobscura@gmail.com>
2025-12-07 17:07:09 +0100

    day 3 part 2

--- day3/day3.ex
+++ day3/day3.ex
@@ -30,6 +30,22 @@ defmodule AOC do
     digits = Enum.map(chars, &elem(Integer.parse(&1), 0))
     List.first(Enum.sort(digits, :desc))
   end
+
+  def largest_joltage2(line) do
+    chars = String.graphemes(line)
+    digits = Enum.map(chars, &elem(Integer.parse(&1), 0))
+    find_best_num(digits, 12, 0, [])
+  end
+
+  def find_best_num(digits, rem, pos, batteries) do
+    e = length(digits) - rem + 1
+    best = Enum.max(Enum.slice(digits, pos..(e - 1)))
+    new_pos = Enum.find_index(Enum.slice(digits, pos..(e - 1)), fn x -> x == best end) + 1
+    cond do
+      rem == 1 -> elem(Integer.parse(Enum.join(batteries ++ [best])), 0)
+      true -> find_best_num(digits, rem - 1, new_pos + pos, batteries ++ [best])
+    end
+  end
 end
 
 input_file = File.read!("input.txt")
@@ -38,3 +54,6 @@ lines = String.split(input_file, "\n")
 
 joltages = Enum.map(lines, &AOC.largest_joltage/1)
 IO.inspect(Enum.sum(joltages))
+
+joltages2 = Enum.map(lines, &AOC.largest_joltage2/1)
+IO.inspect(Enum.sum(joltages2))
