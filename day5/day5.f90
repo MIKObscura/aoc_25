280418b10e5b48bb1ac62db6234c4d9d5d2baf26
MIKObscura <contact.mikobscura@gmail.com>
2025-12-10 20:13:25 +0100

    day 5 part 2

--- day5/day5.f90
+++ day5/day5.f90
@@ -2,14 +2,16 @@ program day5
     use iso_fortran_env, only: int64
     implicit none
     character(len=100) :: line
-    integer(kind=int64) :: ios, a, b, i, total_fresh
+    integer(kind=int64) :: ios, a, b, i, j, total_fresh, total_ranges
     integer(kind=int64), allocatable :: fresh_batches(:)
+    integer(kind=int64), allocatable :: fresh_ranges(:, :), merged_ranges(:, :), tmp(:, :)
     integer(kind=int64), allocatable :: ingredients(:)
     integer(kind=int64) :: n_singles
 
     allocate(fresh_batches(0))
     n_singles = 0
     total_fresh = 0
+    total_ranges = 1
 
     open(unit=10, file="input.txt", status="old", action="read")
 
@@ -43,10 +45,32 @@ program day5
     end do
     close(10)
 
-    do i = 1, size(ingredients)
+    do i = 1, size(ingredients) ! Part 1
         if (ingredient_is_fresh(ingredients(i), fresh_batches)) total_fresh = total_fresh + 1
     end do
     print *, total_fresh
+
+    call batches_to_pairs(fresh_batches, fresh_ranges) ! Part 2
+    call sort_ranges(fresh_ranges, size(fresh_ranges, 2))
+    allocate(merged_ranges(2, total_ranges))
+    merged_ranges(:, 1) = fresh_ranges(:, 1)
+    do i = 2, size(fresh_ranges, 2)
+        if (intervals_overlap(merged_ranges(:, size(merged_ranges, 2)), fresh_ranges(:, i))) then
+            merged_ranges(:, size(merged_ranges, 2)) = merge_intervals(merged_ranges(:, size(merged_ranges, 2)), fresh_ranges(:, i))
+        else
+            allocate(tmp(2, total_ranges + 1))
+            tmp(:, 1:total_ranges) = merged_ranges(:, 1:total_ranges)
+            total_ranges = total_ranges + 1
+            tmp(:, total_ranges) = fresh_ranges(:, i)
+            call move_alloc(tmp, merged_ranges)
+        end if
+    end do
+
+    total_fresh = 0
+    do i = 1, size(merged_ranges, 2)
+        total_fresh = total_fresh + (merged_ranges(2, i) - merged_ranges(1, i)) + 1
+    end do
+    print *, total_fresh ! TODO: figure out why the answer isn't exactly right
 
 contains
 
@@ -69,6 +93,43 @@ contains
         call move_alloc(tmp, arr)
     end subroutine append_range
 
+    subroutine batches_to_pairs(batches, pairs)
+        integer(kind=int64), allocatable, intent(in) :: batches(:)
+        integer(kind=int64), allocatable, intent(inout) :: pairs(:, :)
+        integer(kind=int64) :: i, j
+
+        allocate(pairs(2, size(batches) / 2))
+        j = 1
+        do i = 1, size(batches), 2
+            pairs(1, j) = batches(i)
+            pairs(2, j) = batches(i + 1)
+            j = j + 1
+        end do
+
+    end subroutine batches_to_pairs
+
+    subroutine sort_ranges(ranges, n)
+        integer, intent(in) :: n
+        integer(kind=int64), intent(inout) :: ranges(2, n)
+        integer(kind=int64) :: i, j
+        real :: key1, key2
+
+        do i = 2, n
+            key1 = ranges(1, i)
+            key2 = ranges(2, i)
+            j = i - 1
+
+            do while (j >= 1 .and. ranges(1, j) > key1)
+                ranges(1, j+1) = ranges(1, j)
+                ranges(2, j+1) = ranges(2, j)
+                j = j - 1
+            end do
+
+            ranges(1, j+1) = key1
+            ranges(2, j+1) = key2
+        end do
+    end subroutine sort_ranges
+
     function ingredient_is_fresh(ingredient, fresh_batches) result(res)
         integer(kind=int64) :: ingredient,i
         integer(kind=int64) :: fresh_batches(:)
@@ -81,6 +142,22 @@ contains
                 res = .TRUE.
             end if
         end do
+    end function
+
+    function intervals_overlap(interval1, interval2) result(res)
+        integer(kind=int64), dimension(2) :: interval1, interval2
+        logical :: res
+
+        res = .FALSE.
+        if (interval1(2) >= interval2(1)) then
+            res = .TRUE.
+        end if
+    end function
+
+    function merge_intervals(interval1, interval2) result(res)
+        integer(kind=int64), dimension(2) :: interval1, interval2, res
+
+        res = [minval([interval1(1), interval2(1)]), maxval([interval1(2), interval2(2)])]
     end function
 
 end program day5
