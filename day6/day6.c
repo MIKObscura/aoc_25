505626992d80c30c43e3f2a83cfffd6b82c70e56
MIKObscura <contact.mikobscura@gmail.com>
2025-12-11 21:25:31 +0100

    day 6 part 2

--- day6/day6.c
+++ day6/day6.c
@@ -13,14 +13,37 @@ void rmspaces(char *str) {
     str[count] = '\0';
 }
 
+int findlongestline(char **lines, int linenum){
+    int longest = 0;
+    for(int i = 0; i < linenum; i++){
+        if(strlen(lines[i]) >= strlen(lines[longest])){
+            longest = i;
+        }
+    }
+    return longest;
+}
+
 long applyop(char op, int **nums, int column, int total_lines){
-    long long total = nums[0][column];
+    long total = nums[0][column];
 
     for(int i = 1; i < total_lines; i++){
         if(op == '+'){
-            total += (long long) nums[i][column];
+            total += (long) nums[i][column];
         } else if(op == '*'){
-            total *= (long long) nums[i][column];
+            total *= (long) nums[i][column];
+        }
+    }
+    return total;
+}
+
+long applyvertop(char op, int *nums){
+    long total = nums[0];
+
+    for(int i = 1; i <= (sizeof(nums) / sizeof(int)); i++){
+        if(op == '+'){
+            total += (long) nums[i];
+        } else if(op == '*'){
+            total *= (long) nums[i];
         }
     }
     return total;
@@ -51,6 +74,8 @@ int main(int argc, char* argv[]){
         total_lines++;
         lines = reallocarray(lines, total_lines + 1, sizeof(char *));
     }
+    char* untrimmedline = (char *) malloc(sizeof(char) * strlen(lines[total_lines - 1]));
+    strcpy(untrimmedline, lines[total_lines - 1]);
     rmspaces(lines[total_lines - 1]);
     int columns = strlen(lines[total_lines - 1]);
 
@@ -82,5 +107,48 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < columns; i++){
         total += applyop(operators[i], nums, i, total_lines - 1);
     }
-    printf("%lld", total);
+    printf("%lld \n", total);
+
+    int **vertnums = (int **) malloc(sizeof(int *));
+    int currop = 0;
+    int opsindexes[strlen(operators)];
+    for(int i = 0; i < strlen(untrimmedline); i++){
+        if(untrimmedline[i] == ' ') continue;
+        opsindexes[currop] = i;
+        currop++;
+    }
+    for(int i = 0; i < strlen(operators); i++){
+        int opi = opsindexes[i];
+        int numlen;
+        int *colnums = (int *) malloc(sizeof(int));
+        int longestline = findlongestline(lines, total_lines);
+        int colnumsi = 0;
+        if(i == strlen(operators) - 1){
+            numlen = strlen(lines[longestline]) - opi;
+        } else{
+            numlen = (opsindexes[i+1] - 1) - opi;
+        }
+        for(int ii = numlen - 1; ii >= 0; ii--){
+            char *newnum = (char *) malloc(sizeof(char));
+            for(int iii = 0; iii < total_lines - 1; iii++){
+                if(strlen(lines[iii]) - 1 < opi + ii){
+                    newnum[iii] = ' ';
+                    continue;
+                }
+                newnum[iii] = lines[iii][opi+ii];
+            }
+            rmspaces(newnum);
+            colnums[colnumsi] = atoi(newnum);
+            colnumsi++;
+            colnums = reallocarray(colnums, colnumsi + 1, sizeof(int));
+        }
+        vertnums[i] = colnums;
+        vertnums = reallocarray(vertnums, i + 2, sizeof(int *));
+    }
+
+    long long total2 = 0; // TODO: figure out why the answer is wrong
+    for(int i = 0; i < columns; i++){
+        total2 += applyvertop(operators[i], vertnums[i]);
+    }
+    printf("%lld \n", total2);
 }
