c7c12431aa7115ad1e47230aac14f16a0cca0035
MIKObscura <contact.mikobscura@gmail.com>
2025-12-13 16:54:21 +0100

    day 7 part 1

---
+++ day7/day7.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+
+int main(int argc, char* argv[]){
+    std::ifstream input_file("input.txt");
+    std::string line;
+    std::vector<std::vector<char>> board;
+
+    while(getline(input_file, line)){
+        std::vector<char> line_char(line.begin(), line.end());
+        board.push_back(line_char);
+    }
+
+    std::pair<int, int> start_coord;
+    for(int i = 0; i < board[0].size(); i++){
+        if(board[0][i] == 'S'){
+            start_coord = std::make_pair(0, i);
+            break;
+        }
+    }
+
+    int splits = 0;
+    int curr_row = 1;
+    std::vector<std::pair<int, int>> curr_beams = {start_coord};
+    for(;;){
+        if(curr_row == board.size()) break;
+        std::vector<std::pair<int, int>> new_beams;
+        for(auto it = curr_beams.begin(); it != curr_beams.end(); it++){
+            auto next_pos = std::make_pair(curr_row, it->second);
+            if(board[next_pos.first][next_pos.second] == '^'){
+                new_beams.push_back(std::make_pair(curr_row, it->second + 1));
+                new_beams.push_back(std::make_pair(curr_row, it->second - 1));
+                splits++;
+            } else{
+                new_beams.push_back(std::make_pair(curr_row, it->second));
+            }
+        }
+        std::set<std::pair<int, int>> tmp_beams(new_beams.begin(), new_beams.end());
+        new_beams.assign(tmp_beams.begin(), tmp_beams.end());
+        curr_beams = new_beams;
+        curr_row++;
+    }
+    std::cout << splits << std::endl;
+}
